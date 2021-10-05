0036af7cf4a0c21e5757eae31d5c715a4dc723ac
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-10-05 16:40:05 -0400

    added when method to question and validated options based on boolean true or false

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { truncate } = require('fs');
 const inquirer = require('inquirer');
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
@@ -41,9 +42,22 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
